e630f87769f2e0e3abbefe1b444d8ba8bc25fb85
Gonza Iglesias <gonns.iglesias@gmail.com>
2021-06-08 22:46:40 -0300

    Add api client

--- src/screens/ItemsScreen.tsx
+++ src/screens/ItemsScreen.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Paragraph } from 'react-native-paper';
 import Header from '../components/Header';
 import Background from '../components/Background';
 import ItemsList from '../components/ItemsList';
 import NavBar from '../components/NavBar';
-
-const items = [
-  {
-    name: 'Tasa de te',
-    description: 'Tasa de te heredada',
-    imageUrl: ''
-  },
-  {
-    name: 'Tetera',
-    description: 'Tetera heredada',
-    imageUrl: ''
-  }
-];
+import client from '../client/client';
 
 const ItemsScreen = ({ navigation }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    const search = async () => {
+      const { data } = await client.get('/products');
+      setItems(data);
+    };
+    search();
+  }, []);
   return (
     <>
       <NavBar goBack={() => navigation.goBack()} title="Subastas"></NavBar>
