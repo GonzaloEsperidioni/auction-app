a4b7541e2b741f11ce27ffe9b26d57172ce529ec
Gonzalo Esperidioni <gonzaloesperidioni@gmail.com>
2021-06-10 17:45:21 -0300

    cambios

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,31 +1,38 @@
-import React, { memo } from 'react';
+import React, { memo, useContext } from 'react';
 import Background from '../components/Background';
 import Logo from '../components/Logo';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import Paragraph from '../components/Paragraph';
 import { Navigation } from '../types';
+import AutionContext from '../context/AutionContext';
 
 type Props = {
   navigation: Navigation;
 };
 
-const HomeScreen = ({ navigation }: Props) => (
-  <Background>
-    <Logo />
-    <Header>SubastApp</Header>
+const HomeScreen = ({ navigation }: Props) => {
+  const { authenticated, setAuthenticated } = useContext(AutionContext);
+  const login = () => {
+    setAuthenticated(true);
+  };
+  return (
+    <Background>
+      <Logo />
+      <Header>SubastApp</Header>
 
-    <Paragraph>La mejor forma de subastar online.</Paragraph>
-    <Button mode="contained" onPress={() => navigation.navigate('Login')}>
-      Login
-    </Button>
-    <Button mode="outlined" onPress={() => navigation.navigate('Register')}>
-      Registrarse
-    </Button>
-    <Button mode="text" onPress={() => navigation.navigate('Login')}>
-      Ingresar como invitado
-    </Button>
-  </Background>
-);
+      <Paragraph>La mejor forma de subastar online.</Paragraph>
+      <Button mode="contained" onPress={() => navigation.navigate('Login')}>
+        Login
+      </Button>
+      <Button mode="outlined" onPress={() => navigation.navigate('Register')}>
+        Registrarse
+      </Button>
+      <Button mode="text" onPress={login}>
+        Ingresar como invitado
+      </Button>
+    </Background>
+  );
+};
 
 export default memo(HomeScreen);
